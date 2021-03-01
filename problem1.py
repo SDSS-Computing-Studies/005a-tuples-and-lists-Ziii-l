f33b169828d2aac1ffe1a7780c03da3f13d84290
Ziii-l <ziweil101@deltalearns.ca>
2021-03-01 14:05:38 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -23,7 +23,7 @@ myList = ["Alain","Brian","Chris","Justin","Angela","Rick"]
 print(myList)
 
 word =str(input("Choose a person from the list to replace: ").strip())
-replace = str(input("Enter the replacement: "))
+replace = str(input("Enter the replacement: "))
 
 dIndex = myList.index(word)
 myList.remove(word)
